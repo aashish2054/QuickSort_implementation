a29db5ebc22090dbd0a717c77609cc11db9b05bf
aashish2054 <ashish.chaudhary@knoldus.com>
2021-03-05 23:27:06 +0530

    Quick Sort implementation in Scala

---
+++ Quick-Sort/src/main/scala/QuickSort.scala
@@ -0,0 +1,9 @@
+object QuickSort {
+  def main(args: Array[String]): Unit = {
+    val QuickSort = new Sorting
+
+    //function is called here to perform sorting operation
+    println("Sorted List is: " + QuickSort.Sorting(List(78, 32, 1, 587, 344, 20, 333)))
+  }
+
+}
