4f31fef910f93c0cb202d396bdbcd733066dd65b
aashish2054 <77047591+aashish2054@users.noreply.github.com>
2021-03-12 00:40:21 +0530

    Delete Quick-Sort directory

--- Quick-Sort/src/main/scala/QuickSort.scala
+++
@@ -1,9 +0,0 @@
-object QuickSort {
-  def main(args: Array[String]): Unit = {
-    val QuickSort = new Sorting
-
-    //function is called here to perform sorting operation
-    println("Sorted List is: " + QuickSort.Sorting(List(78, 32, 1, 587, 344, 20, 333)))
-  }
-
-}
